9570fa19954e3b4b675254e2c6fc3f083595b58e
korken89 <ltu.korken@gmail.com>
2014-04-29 17:47:45 +0200

    Disabled polled SPI

--- halconf.h
+++ halconf.h
@@ -296,7 +296,7 @@
  * @note    Disabling this option saves both code and data space.
  */
 #if !defined(SPI_USE_WAIT) || defined(__DOXYGEN__)
-#define SPI_USE_WAIT                TRUE
+#define SPI_USE_WAIT                FALSE
 #endif
 
 /**
