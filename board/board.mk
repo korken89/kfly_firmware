9e09de5e4ae468b65aa83e327e93516509fbd723
korken89 <ltu.korken@gmail.com>
2014-05-03 15:27:24 +0200

    Makefile fix

--- board/board.mk
+++ board/board.mk
@@ -1,5 +1,6 @@
 # List of all the board related files.
-BOARDSRC = board/board.c board/mpu6050.c board/hmc5983.c
+BOARDSRC = board/board.c board/mpu6050.c board/hmc5983.c \
+           board/sensor_read.c
 
 # Required include directories
 BOARDINC = board/
