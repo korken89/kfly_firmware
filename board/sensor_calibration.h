eaa3ae3594a56050c2ad653f2f85fbc0ff0c14b5
korken89 <ltu.korken@gmail.com>
2014-05-03 15:27:17 +0200

    Added sensor calibration

---
+++ board/sensor_calibration.h
@@ -0,0 +1,18 @@
+#ifndef __SENSOR_CALIBRATION_H
+#define __SENSOR_CALIBRATION_H
+
+/* Defines */
+
+/* Typedefs */
+
+/* Global variable defines */
+typedef struct
+{
+	float bias[3];
+	float gain[3];
+} Sensor_Calibration;
+
+/* Global function defines */
+
+
+#endif
