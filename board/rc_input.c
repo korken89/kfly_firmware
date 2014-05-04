8121e41fd27473742bc0179eb6db015112426580
korken89 <ltu.korken@gmail.com>
2014-05-04 21:17:11 +0200

    Added framework for RC Inputs

---
+++ board/rc_input.c
@@ -0,0 +1,29 @@
+/* *
+ *
+ * Abstraction Layer for RC Outputs
+ *
+ * */
+
+#include "ch.h"
+#include "hal.h"
+#include "board.h"
+#include "rc_input.h"
+
+/* Global variable defines */
+
+/* Private function defines */
+
+/* Private external functions */
+
+/**
+ * @brief Initializes RC inputs
+ * 
+ * @param[in] cfg Pointer to configuration structure
+ * @return RDY_OK if the initialization was successful
+ */
+msg_t RCInputInit()
+{
+	msg_t status = RDY_OK;
+
+	return status;
+}
