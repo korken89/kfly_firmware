9910920597b339129488def6c352cb4285011a8f
korken89 <ltu.korken@gmail.com>
2014-05-05 10:15:00 +0200

    More outline code for EICU

--- board/extended_icu.c
+++ board/extended_icu.c
@@ -6,11 +6,103 @@
 
 #include "ch.h"
 #include "hal.h"
-#include "board.h"
 #include "extended_icu.h"
 
-/* Global variable defines */
+/*===========================================================================*/
+/* Driver local definitions.                                                 */
+/*===========================================================================*/
 
-/* Private function defines */
+/*===========================================================================*/
+/* Driver exported variables.                                                */
+/*===========================================================================*/
+/**
+ * @brief   EICUD1 driver identifier.
+ * @note    The driver EICUD1 allocates the complex timer TIM1 when enabled.
+ */
+#if STM32_EICU_USE_TIM1 && !defined(__DOXYGEN__)
+EICUDriver EICUD1;
+#endif
 
-/* Private external functions */
+/**
+ * @brief   EICUD2 driver identifier.
+ * @note    The driver EICUD2 allocates the timer TIM2 when enabled.
+ */
+#if STM32_EICU_USE_TIM2 && !defined(__DOXYGEN__)
+EICUDriver EICUD2;
+#endif
+
+/**
+ * @brief   EICUD3 driver identifier.
+ * @note    The driver EICUD3 allocates the timer TIM3 when enabled.
+ */
+#if STM32_EICU_USE_TIM3 && !defined(__DOXYGEN__)
+EICUDriver EICUD3;
+#endif
+
+/**
+ * @brief   EICUD4 driver identifier.
+ * @note    The driver EICUD4 allocates the timer TIM4 when enabled.
+ */
+#if STM32_EICU_USE_TIM4 && !defined(__DOXYGEN__)
+EICUDriver EICUD4;
+#endif
+
+/**
+ * @brief   EICUD5 driver identifier.
+ * @note    The driver EICUD5 allocates the timer TIM5 when enabled.
+ */
+#if STM32_EICU_USE_TIM5 && !defined(__DOXYGEN__)
+EICUDriver EICUD5;
+#endif
+
+/**
+ * @brief   EICUD8 driver identifier.
+ * @note    The driver EICUD8 allocates the timer TIM8 when enabled.
+ */
+#if STM32_EICU_USE_TIM8 && !defined(__DOXYGEN__)
+EICUDriver EICUD8;
+#endif
+
+/**
+ * @brief   EICUD9 driver identifier.
+ * @note    The driver EICUD9 allocates the timer TIM9 when enabled.
+ */
+#if STM32_EICU_USE_TIM9 && !defined(__DOXYGEN__)
+EICUDriver EICUD9;
+#endif
+
+/**
+ * @brief   EICUD12 driver identifier.
+ * @note    The driver EICUD12 allocates the timer TIM12 when enabled.
+ */
+#if STM32_EICU_USE_TIM12 && !defined(__DOXYGEN__)
+EICUDriver EICUD12;
+#endif
+
+/*===========================================================================*/
+/* Driver local variables and types.                                         */
+/*===========================================================================*/
+
+/*===========================================================================*/
+/* Driver local functions.                                                   */
+/*===========================================================================*/
+/**
+ * @brief   Shared IRQ handler.
+ *
+ * @param[in] icup      pointer to the @p ICUDriver object
+ */
+static void eicu_lld_serve_interrupt(EICUDriver *eicup)
+{
+	uint16_t sr;
+	sr = eicup->tim->SR;
+	eicup->tim->SR = ~sr;
+}
+
+/*===========================================================================*/
+/* Driver interrupt handlers.                                                */
+/*===========================================================================*/
+
+
+/*===========================================================================*/
+/* Driver exported functions.                                                */
+/*===========================================================================*/
--- board/extended_icu.h
+++ board/extended_icu.h
@@ -32,21 +32,31 @@
 
 #define EICU_SlaveMode_Reset				((uint16_t)0x0004)
 
-
 /* Global variable defines */
 
 
 /* Typedefs */
-/**
- * @brief   EICU notification callback type.
- *
- * @param[in] eicup      pointer to a EICUDriver object
+/** 
+ * @brief	EICU channel selection definition
  */
-typedef void (*eicucallback_t)(ICUDriver *eicup);
+typedef enum {
+	EICU_Channel_1 = 0,
+	EICU_Channel_2 = 1,
+	EICU_Channel_3 = 2,
+	EICU_Channel_4 = 3,
+} EICU_Channel;
+
+/**
+ * @brief	EICU notification callback type.
+ *
+ * @param[in] eicup 	Pointer to a EICUDriver object
+ * @param[in] channel 	EICU channel that fired the interrupt
+ */
+typedef void (*eicucallback_t)(ICUDriver *eicup, EICU_Channel channel);
 
 /** 
-  * @brief  EICU Time Base Settings structure definition
-  */
+ * @brief	EICU Time Base Settings structure definition
+ */
 typedef struct
 {
 	uint16_t EICU_Prescaler; /* Specifies the prescaler value used to divide
@@ -58,8 +68,8 @@ typedef struct
 } EICU_TimeBase_Settings; 
 
 /** 
-  * @brief  EICU Input Capture Settings structure definition  
-  */
+ * @brief	EICU Input Capture Settings structure definition  
+ */
 typedef struct
 {
 	uint16_t EICU_ICPolarity;	/* Specifies the active edge of the input
@@ -73,12 +83,16 @@ typedef struct
 								   parameter can be a number between
 								   0x0 and 0xF */
 
-	eicucallback_t cb;			/* Input capture event callback */
+	eicucallback_t capture_cb;	/* Capture event callback */
+
+	eicucallback_t period_cb;	/* Period capture event callback */
+
+	eicucallback_t overflow_cb;	/* Timer overflow event callback */
 } EICU_IC_Settings;
 
 /** 
-  * @brief  EICU Input Capture Config structure definition  
-  */
+ * @brief	EICU Input Capture Config structure definition  
+ */
 typedef struct
 {
 	bool_t pwm_measurement;				/* PWM Measurement can be
@@ -98,22 +112,59 @@ typedef struct
 										   Channel 2 may be used. */
 } EICU_Config;
 
+/** 
+ * @brief	EICU Input Capture Driver structure definition  
+ */
 typedef struct
 {
-	stm32_tim_t tim;					/* Timer peripheral for Input Capture */
+	stm32_tim_t *tim;					/* Timer peripheral for
+										   Input Capture. */
 
 	uint32_t clock;						/* Timer base clock. */
 
 	EICU_Config *cfg;					/* Pointer to configuration for the
-										   driver */
+										   driver. */
 
-	volatile uint32_t *wccrp;			/* CCR register for width capture */
+	volatile uint32_t *wccrp[4];		/* CCR registers for width capture. */
 
-	volatile uint32_t *pccrp;			/* CCR register for period capture */
+	volatile uint32_t *pccrp;			/* CCR register for period capture.
+										   Only one is needed since only one
+										   PWM input per timer is allowed. */
 } EICUDriver;
 
 
+/* Global Variable Defines */
+#if STM32_EICU_USE_TIM1 && !defined(__DOXYGEN__)
+extern EICUDriver EICUD1;
+#endif
 
+#if STM32_EICU_USE_TIM2 && !defined(__DOXYGEN__)
+extern EICUDriver EICUD2;
+#endif
+
+#if STM32_EICU_USE_TIM3 && !defined(__DOXYGEN__)
+extern EICUDriver EICUD3;
+#endif
+
+#if STM32_EICU_USE_TIM4 && !defined(__DOXYGEN__)
+extern EICUDriver EICUD4;
+#endif
+
+#if STM32_EICU_USE_TIM5 && !defined(__DOXYGEN__)
+extern EICUDriver EICUD5;
+#endif
+
+#if STM32_EICU_USE_TIM8 && !defined(__DOXYGEN__)
+extern EICUDriver EICUD8;
+#endif
+
+#if STM32_EICU_USE_TIM9 && !defined(__DOXYGEN__)
+extern EICUDriver EICUD9;
+#endif
+
+#if STM32_EICU_USE_TIM12 && !defined(__DOXYGEN__)
+extern EICUDriver EICUD12;
+#endif
 
 /* Global function defines */
 
