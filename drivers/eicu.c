d61a9996d6174f578eaeb2cc61c7c736c3d50ed1
korken89 <ltu.korken@gmail.com>
2014-05-13 11:39:52 +0200

    Added the info that the code is from ChibiOS and rewritten by me.

--- drivers/eicu.c
+++ drivers/eicu.c
@@ -1,10 +1,28 @@
+/*
+    ChibiOS/RT - Copyright (C) 2006-2013 Giovanni Di Sirio
+
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+        http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+*/
+/*
+   Rewritten by Emil Fresk (1/5 - 2014) for extended input capture
+   functionallity. And fix for spourious callbacks in the interrupt handler.
+*/
+
 /* *
  *
  * Hardware Abstraction Layer for Extended Input Capture Unit
  *
  * */
-
-
 
 #include "ch.h"
 #include "hal.h"
