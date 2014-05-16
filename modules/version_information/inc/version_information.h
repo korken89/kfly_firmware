f21c6da46d703409dae1ed8224c966264e96e099
korken89 <ltu.korken@gmail.com>
2014-05-16 18:38:58 +0200

    Fixed indentation

--- modules/version_information/inc/version_information.h
+++ modules/version_information/inc/version_information.h
@@ -2,24 +2,24 @@
 #define __VERSION_INFORMATION_H
 
 /* Defines */
-#define UNIQUE_ID_SIZE			12
-#define VERSION_MAX_SIZE		70
-#define USER_ID_MAX_SIZE		100
+#define UNIQUE_ID_SIZE          12
+#define VERSION_MAX_SIZE        70
+#define USER_ID_MAX_SIZE        100
 
-#define UNIQUE_ID_BASE			0x1fff7a10
-#define FIRMWARE_BASE			0x08000000
-#define BOOTLOADER_BASE			0x08000000
-#define SW_VERSION_OFFSET		0x1c0
+#define UNIQUE_ID_BASE          0x1fff7a10
+#define FIRMWARE_BASE           0x08000000
+#define BOOTLOADER_BASE         0x08000000
+#define SW_VERSION_OFFSET       0x1c0
 
-#ifndef DATE
-	#define DATE 			"no timestamp"
+#ifndef GIT_DATE
+    #define GIT_DATE            "no timestamp"
 #endif
 
 #ifndef GIT_VERSION
-	#define GIT_VERSION 	"no version"
+    #define GIT_VERSION         "no version"
 #endif
 
-#define KFLY_VERSION	GIT_VERSION ", Build date: " DATE "\0"
+#define KFLY_VERSION    GIT_VERSION ", Build date: " GIT_DATE "\0"
 
 /* Typedefs */
 
--- modules/version_information/src/version_information.c
+++ modules/version_information/src/version_information.c
@@ -26,20 +26,20 @@ void VersionInformationInit(void)
 
 uint8_t *ptrGetUniqueID(void)
 {
-	return (uint8_t *)(UNIQUE_ID_BASE);
+    return (uint8_t *)(UNIQUE_ID_BASE);
 }
 
 uint8_t *ptrGetBootloaderVersion(void)
 {
-	return (uint8_t *)(BOOTLOADER_BASE + SW_VERSION_OFFSET);
+    return (uint8_t *)(BOOTLOADER_BASE + SW_VERSION_OFFSET);
 }
 
 uint8_t *ptrGetFirmwareVersion(void)
 {
-	return (uint8_t *)(FIRMWARE_BASE + SW_VERSION_OFFSET);
+    return (uint8_t *)(FIRMWARE_BASE + SW_VERSION_OFFSET);
 }
 
 uint8_t *ptrGetUserIDString(void)
 {
-	return UserIDString;
+    return UserIDString;
 }
