1b781a2b7c4acfd6be98ef40a605fbd565a45c62
korken89 <ltu.korken@gmail.com>
2014-05-30 15:55:45 +0200

    Started redesign of external flash handling

--- modules/external_flash/inc/ext_flash.h
+++ modules/external_flash/inc/ext_flash.h
@@ -14,22 +14,26 @@
 /* Includes */
 
 /* Defines */
-#define FLASH_SYNC_WORD         0xdeadbeef  /* Sync word to detect errors in the flash structure */
+#define FLASH_SYNC_WORD         0xdeadbeef  /* Sync word to detect errors in
+                                               the flash structure */
 #define FLASH_SYNC_WORD_REV     0xefbeadde
 
-#define FLASH_CMD_WRSR          0x01        /* Write Status Register instruction    */
-#define FLASH_CMD_PAGE_PROGRAM  0x02        /* Write to Memory instruction          */
-#define FLASH_CMD_READ          0x03        /* Read from Memory instruction         */
-#define FLASH_CMD_RDSR          0x05        /* Read Status Register instruction     */
-#define FLASH_CMD_WREN          0x06        /* Write enable instruction             */
-#define FLASH_CMD_PAGE_WRITE    0x0A        /* Writes a page with automatic erase   */
+#define FLASH_CMD_WRSR          0x01        /* Write Status Register
+                                               instruction */
+#define FLASH_CMD_PAGE_PROGRAM  0x02        /* Write to Memory instruction */
+#define FLASH_CMD_READ          0x03        /* Read from Memory instruction */
+#define FLASH_CMD_RDSR          0x05        /* Read Status Register
+                                               instruction */
+#define FLASH_CMD_WREN          0x06        /* Write enable instruction */
+#define FLASH_CMD_PAGE_WRITE    0x0A        /* Writes a page with automatic 
+                                               erase */
 
-#define FLASH_CMD_BE            0xC7        /* Bulk Erase instruction               */
-#define FLASH_CMD_SE            0xD8        /* Sector Erase instruction             */
-#define FLASH_CMD_PE            0xDB        /* Page erase                           */
-#define FLASH_CMD_RDID          0x9F        /* Read identification                  */
+#define FLASH_CMD_BE            0xC7        /* Bulk Erase instruction */
+#define FLASH_CMD_SE            0xD8        /* Sector Erase instruction */
+#define FLASH_CMD_PE            0xDB        /* Page erase */
+#define FLASH_CMD_RDID          0x9F        /* Read identification */
 
-#define FLASH_WIP_FLAG          0x01        /* Write In Progress (WIP) flag         */
+#define FLASH_WIP_FLAG          0x01        /* Write In Progress (WIP) flag */
 
 #define FLASH_M25P128_ID        0x202018
 #define FLASH_M25P64_ID         0x202017
@@ -57,27 +61,24 @@
 #define FLASH_END               0
 
 /* Typedefs */
-typedef struct {
-    uint8_t *ptr;       /* Holds the pointer of where the data should be saved to/read from */
-    int32_t count;      /* Holds the number of bytes to write/read */
-} Flash_Save_Template;
-
 
 /* Macros */
 #define FLASH_CS_LOW()          palClearPad(GPIOC, GPIOC_FLASH_SEL)
 #define FLASH_CS_HIGH()         palSetPad(GPIOC, GPIOC_FLASH_SEL)
+#define FLASH_STR_TO_ID(str)    (((uint32_t)((str)[0]) << 0) | \
+                                ((uint32_t)((str)[1]) << 8) | \
+                                ((uint32_t)((str)[2]) << 16) | \
+                                ((uint32_t)((str)[3]) << 24))
+#define FLASH_BYTES_TO_ID(a, b, c, d)   (((uint32_t)(a) << 0) | \
+                                        ((uint32_t)(b) << 8) | \
+                                        ((uint32_t)(c) << 16) | \
+                                        ((uint32_t)(d) << 24))
 
 /* Global functions */
 void ExternalFlashInit(void);
 void ExternalFlash_EraseBulk(void);
 void ExternalFlash_EraseSector(uint32_t sector);
 uint32_t ExternalFlash_ReadID(void);
-ErrorStatus ExternalFlash_CheckIntegrity(const Flash_Save_Template *template,
-                                         uint32_t sector);
-ErrorStatus ExternalFlash_SaveSettings(const Flash_Save_Template *template,
-                                       uint32_t sector);
-ErrorStatus ExternalFlash_LoadSettings(const Flash_Save_Template *template,
-                                       uint32_t sector);
 void ExternalFlash_WritePage(uint8_t *buffer,
                              uint32_t address, 
                              uint16_t count);
@@ -85,6 +86,6 @@ void ExternalFlash_ReadBuffer(uint8_t *buffer,
                              uint32_t address, 
                              uint16_t count);
 void ExternalFlash_WriteEnable(void);
-void ExternalFlash_WaitForWriteEnd(void);
+void ExternalFlash_WaitForWriteEnd(uint32_t delay_ms);
 
 #endif
--- modules/external_flash/src/ext_flash.c
+++ modules/external_flash/src/ext_flash.c
@@ -11,22 +11,17 @@
 #include "control.h"
 #include "rc_input.h"
 
+/* Claim and release macros for external flash memory */
+#define ExternalFlashClaim()     chMtxLock(&ext_flash_lock)
+#define ExternalFlashRelease()   chMtxUnlock(&ext_flash_lock)
+
+
 /* Global variable defines */
 
 /* Private variable defines */
-
-/* Area to hold the design of the external flash memory */
-static const Flash_Save_Template Flash_Save_Structure[] = {
-    {   /* To indicate the end of the save structure */
-        .ptr = FLASH_END,
-        .count = FLASH_END
-    }
-};
+static mutex_t ext_flash_lock;
 
 /* Private function defines */
-static uint32_t SaveStructure_NumberOfBytes(const Flash_Save_Template *template);
-static void SaveStructure_WriteToMemory(const Flash_Save_Template *template,
-                                        uint32_t sector);
 
 /* Private external functions */
 
@@ -34,6 +29,8 @@ static void SaveStructure_WriteToMemory(const Flash_Save_Template *template,
 void ExternalFlashInit(void)
 {
     uint32_t id;
+
+    chMtxObjectInit(&ext_flash_lock);
 
     id = ExternalFlash_ReadID();
 
@@ -43,6 +40,8 @@ void ExternalFlashInit(void)
 
 void ExternalFlash_EraseBulk(void)
 {
+    ExternalFlashClaim();
+
     /* Enable the write access to the External Flash */
     ExternalFlash_WriteEnable();
 
@@ -56,7 +55,9 @@ void ExternalFlash_EraseBulk(void)
     FLASH_CS_HIGH();
 
     /* Wait the end of Flash writing */
-    ExternalFlash_WaitForWriteEnd();
+    ExternalFlash_WaitForWriteEnd(100);
+
+    ExternalFlashRelease();
 }
 
 void ExternalFlash_EraseSector(uint32_t sector)
@@ -79,7 +80,7 @@ void ExternalFlash_EraseSector(uint32_t sector)
     FLASH_CS_HIGH();
 
     /* Wait the end of Flash writing */
-    ExternalFlash_WaitForWriteEnd();
+    ExternalFlash_WaitForWriteEnd(100);
 }
 
 /**
@@ -89,7 +90,7 @@ void ExternalFlash_EraseSector(uint32_t sector)
  */
 uint32_t ExternalFlash_ReadID(void)
 {
-    uint32_t t1 = 0, t2 = 0, t3 = 0;
+    uint32_t t1, t2, t3;
 
     /* Select the External Flash: Chip Select low */
     FLASH_CS_LOW();
@@ -106,89 +107,6 @@ uint32_t ExternalFlash_ReadID(void)
     FLASH_CS_HIGH();
 
     return ((t1 << 16) | (t2 << 8) | t3);
-}
-
-
-ErrorStatus ExternalFlash_CheckIntegrity(const Flash_Save_Template *template,
-                                         uint32_t sector)
-{
-    uint32_t i, page_address;
-    union
-    {
-        uint8_t data[4];
-        uint32_t value;
-    } sync_data;
-
-    i = 0;
-
-    /* Calculate the starting sector address */
-    page_address = sector * FLASH_SECTOR_SIZE;
-
-    /* Check so we read from one of the sectors */
-    if (sector > FLASH_NUM_SECTORS)
-        return ERROR;
-
-    while (template[i].ptr != FLASH_END)
-    {
-        /* Read the sync word from the flash */
-        ExternalFlash_ReadBuffer(sync_data.data, page_address, 4);
-
-        /* If the sync word is not correct the memory is fragmented.
-           Return error. */
-        if (sync_data.value != FLASH_SYNC_WORD_REV)
-            return ERROR;
-
-        /* Increment Flash address and template address */
-        page_address += template[i].count + 4; /* The 4 comes from the SYNC */
-        i++;
-    }
-
-    return SUCCESS;
-}
-
-
-ErrorStatus ExternalFlash_SaveSettings(const Flash_Save_Template *template,
-                                       uint32_t sector)
-{
-    /* Check so we write to one of the sectors */
-    if (sector > FLASH_NUM_SECTORS)
-        return ERROR;
-
-    /* Erase the selected sector */
-    ExternalFlash_EraseSector(sector * FLASH_SECTOR_SIZE);
-
-    /* Start saving data to the external flash memory */
-    SaveStructure_WriteToMemory(template, sector);
-
-    return SUCCESS;
-}
-
-
-ErrorStatus ExternalFlash_LoadSettings(const Flash_Save_Template *template, 
-                                       uint32_t sector)
-{
-    uint32_t i, page_address;
-
-    i = 0;
-
-    /* Calculate the starting sector address */
-    page_address = sector * FLASH_SECTOR_SIZE + 4;
-
-    /* Check so we read from one of the sectors */
-    if (sector > FLASH_NUM_SECTORS)
-        return ERROR;
-
-    while (template[i].ptr != FLASH_END)
-    {
-        /* Read the setting from the flash */
-        ExternalFlash_ReadBuffer(template[i].ptr, page_address, template[i].count);
-
-        /* Increment Flash address and template address */
-        page_address += template[i].count + 4; /* The 4 comes from the SYNC */
-        i++;
-    }
-
-    return SUCCESS;
 }
 
 /**
@@ -230,7 +148,7 @@ void ExternalFlash_WritePage(uint8_t *buffer,
     spiReleaseBus(EXTERNAL_FLASH_SPI);
 
     /* Wait the end of Flash writing */
-    ExternalFlash_WaitForWriteEnd();
+    ExternalFlash_WaitForWriteEnd(1);
 }
 
 /**
@@ -293,124 +211,32 @@ void ExternalFlash_WriteEnable(void)
  * @brief       Polls the status of the Write In Progress (WIP) flag in the External
  *              Flash's status register until write operation has completed.
  */
-void ExternalFlash_WaitForWriteEnd(void)
+void ExternalFlash_WaitForWriteEnd(uint32_t delay_ms)
 {
-    /* Claim the SPI bus */
-    spiAcquireBus(EXTERNAL_FLASH_SPI);
-
-    /* Select the External Flash: Chip Select low */
-    FLASH_CS_LOW();
-
-    /* Send "Read Status Register" instruction */
-    spiPolledExchange(EXTERNAL_FLASH_SPI, FLASH_CMD_RDSR);
+    uint8_t wip;
 
     /* Loop as long as the memory is busy with a write cycle */
-    while (spiPolledExchange(EXTERNAL_FLASH_SPI, FLASH_DUMMY_BYTE) & FLASH_WIP_FLAG);
-
-    /* Deselect the External Flash: Chip Select high */
-    FLASH_CS_HIGH();
-
-    /* Release the SPI bus */
-    spiReleaseBus(EXTERNAL_FLASH_SPI);
-}
-
-
-static uint32_t SaveStructure_NumberOfBytes(const Flash_Save_Template *template)
-{
-    uint32_t i = 0, num_bytes = 0;
-
-    /* While there is an valid pointer add its byte count to the total */
-    while (template[i].ptr != 0)
-        num_bytes += template[i++].count;
-
-    return num_bytes;
-}
-
-/**
- * @brief           Processes a Flash_Save structure to determine the
- *                  correct way of writing it to memory.
- * 
- * @param template  Pointer to the Flash_Save template.
- * @return          Number of bytes to save.
- */
-static void SaveStructure_WriteToMemory(const Flash_Save_Template *template, uint32_t sector)
-{
-    int32_t i, j, num_bytes_written_to_page, page_address;
-
-    i = 0;
-    num_bytes_written_to_page = 0;
-    page_address = sector * FLASH_SECTOR_SIZE;
-
-    /* Enable the write access to the External Flash */
-    ExternalFlash_WriteEnable();
-
-    /* Claim the SPI bus */
-    spiAcquireBus(EXTERNAL_FLASH_SPI);
-
-    /* Select the External Flash: Chip Select low */
-    FLASH_CS_LOW();
-
-    /* Send "Write to Memory" instruction */
-    spiPolledExchange(EXTERNAL_FLASH_SPI, FLASH_CMD_PAGE_PROGRAM);
-
-    /* Send address nibbles for address byte to write to */
-    spiPolledExchange(EXTERNAL_FLASH_SPI, (page_address & 0xFF0000) >> 16);
-    spiPolledExchange(EXTERNAL_FLASH_SPI, (page_address & 0xFF00) >> 8);
-    spiPolledExchange(EXTERNAL_FLASH_SPI, page_address & 0xFF);
-
-    while (template[i].ptr != FLASH_END)
+    do
     {
-        for (j = -4; j < template[i].count; j++)
-        {
-            if (++num_bytes_written_to_page >= FLASH_PAGE_SIZE)
-            {
-                /* Wait for write complete and start a new write cycle */
+        /* If a delay was specified: wait */
+        if (delay_ms != 0)
+            chThdSleep(OSAL_MS2ST(delay_ms));
 
-                /* Deselect the External Flash: Chip Select high */
-                FLASH_CS_HIGH();
+        /* Claim the SPI bus */
+        spiAcquireBus(EXTERNAL_FLASH_SPI);
 
-                /* Wait the end of Flash writing */
-                ExternalFlash_WaitForWriteEnd();
+        /* Select the External Flash: Chip Select low */
+        FLASH_CS_LOW();
 
-                /* Start new write cycle and increment the address */
-                page_address += FLASH_PAGE_SIZE;
+        /* Send "Read Status Register" instruction */
+        spiPolledExchange(EXTERNAL_FLASH_SPI, FLASH_CMD_RDSR);
 
-                /* Enable the write access to the External Flash */
-                ExternalFlash_WriteEnable();
+        wip = spiPolledExchange(EXTERNAL_FLASH_SPI, FLASH_DUMMY_BYTE);
 
-                /* Select the External Flash: Chip Select low */
-                FLASH_CS_LOW();
+        /* Deselect the External Flash: Chip Select high */
+        FLASH_CS_HIGH();
 
-                /* Send "Write to Memory" instruction */
-                spiPolledExchange(EXTERNAL_FLASH_SPI, FLASH_CMD_PAGE_PROGRAM);
-
-                /* Send address nibbles for address byte to write to */
-                spiPolledExchange(EXTERNAL_FLASH_SPI, (page_address & 0xFF0000) >> 16);
-                spiPolledExchange(EXTERNAL_FLASH_SPI, (page_address & 0xFF00) >> 8);
-                spiPolledExchange(EXTERNAL_FLASH_SPI, page_address & 0xFF);
-
-                /* Set to one for the byte to be written directly after */
-                num_bytes_written_to_page = 1;
-            }
-
-            /* Send SYNC first, then the data */
-
-
-            if (j < 0)
-                spiPolledExchange(EXTERNAL_FLASH_SPI, (FLASH_SYNC_WORD >> (-j - 1) * 8) & 0xff);
-            else
-                spiPolledExchange(EXTERNAL_FLASH_SPI, template[i].ptr[j]);
-        }
-
-        i++;
-    }
-
-    /* Deselect the External Flash: Chip Select high */
-    FLASH_CS_HIGH();
-
-    /* Release the SPI bus */
-    spiReleaseBus(EXTERNAL_FLASH_SPI);
-
-    /* Wait the end of Flash writing */
-    ExternalFlash_WaitForWriteEnd();
+        /* Release the SPI bus */
+        spiReleaseBus(EXTERNAL_FLASH_SPI); 
+    } while (wip & FLASH_WIP_FLAG);
 }
