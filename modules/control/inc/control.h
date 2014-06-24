95ac0d5ef63e026a0797e3099758ab95f421ffe3
Emil Fresk <emil.fresk@gmail.com>
2014-06-24 14:52:51 +0200

    Added structures for arm and disarm logic

--- modules/control/inc/control.h
+++ modules/control/inc/control.h
@@ -36,6 +36,41 @@
 /*===========================================================================*/
 
 /**
+ * @brief   Possible stick direction for arming the controllers.
+ */
+typedef enum PACKED_VAR
+{
+    /**
+     * @brief   Arm direction not yet set.
+     */
+    STICK_NONE = 0,
+    /**
+     * @brief   Arm at pitch at min.
+     */
+    STICK_PITCH_MIN,
+    /**
+     * @brief   Arm at pitch at max.
+     */
+    STICK_PITCH_MAX,
+    /**
+     * @brief   Arm at roll at min.
+     */
+    STICK_ROLL_MIN,
+    /**
+     * @brief   Arm at roll at max.
+     */
+    STICK_ROLL_MAX,
+    /**
+     * @brief   Arm at yaw at min.
+     */
+    STICK_YAW_MIN,
+    /**
+     * @brief   Arm at yaw at max.
+     */
+    STICK_YAW_MAX
+} Arming_Stick_Direction;
+
+/**
  * @brief   Possible flight modes the controllers support.
  */
 typedef enum PACKED_VAR
@@ -98,6 +133,35 @@ typedef enum PACKED_VAR
      */
     TARGET_COORDINATE
 } Target_Direcion;
+
+/**
+ * @brief   Settings for the arm and disarm functionality.
+ */
+typedef struct
+{
+    /**
+     * @brief   Throttle threshold for the arm/disarm logic to react.
+     */
+    float throttle_threshold;
+    /**
+     * @brief   Minimum throttle when armed (to spin propellers when armed).
+     */
+    float armed_min_throttle;
+    /**
+     * @brief   Stick direction to arm the controllers.
+     */
+    Arming_Stick_Direction stick_direction;
+    /**
+     * @brief   Time (in seconds) needed to hold the sticks in correct
+     *          position in order to arm the system.
+     */
+    uint8_t arm_stick_time;
+    /**
+     * @brief   Time (in seconds) needed to disarm the controllers if no
+     *          throttle has been given.
+     */
+    uint8_t arm_zero_throttle_timeout;
+} Control_Arm_Settings;
 
 /**
  * @brief   Position, velocity, attitude, rate and actuator
