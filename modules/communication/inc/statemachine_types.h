73601cbb37e62517908972010db59187b1328038
Emil Fresk <emil.fresk@gmail.com>
2014-06-27 18:43:10 +0200

    State machine types migrated to new style

--- modules/communication/inc/statemachine_types.h
+++ modules/communication/inc/statemachine_types.h
@@ -1,107 +1,304 @@
 #ifndef __STATEMACHINE_TYPES_H
 #define __STATEMACHINE_TYPES_H
 
+#include "serialmanager_types.h"
+#include "circularbuffer.h"
+
+/*===========================================================================*/
+/* Module global definitions.                                                */
+/*===========================================================================*/
+
 #define SYNC_BYTE                     (0xa6)
 #define ACK_BIT                       (0x80)
 #define SERIAL_RECIEVE_BUFFER_SIZE    (256)
 #define SERIAL_TRANSMIT_BUFFER_SIZE   (1024)
 
-#include "serialmanager_types.h"
-#include "circularbuffer.h"
+/*===========================================================================*/
+/* Module data structures and types.                                         */
+/*===========================================================================*/
 
-/* These are all the commands for the serial protocol. */
+/**
+ * @brief   All the commands for the serial protocol.
+ */
 typedef enum
 {
+    /**
+     * @brief   The zero command is not allowed.
+     */
     Cmd_None                        = 0,
+    /**
+     * @brief   ACK command.
+     */
     Cmd_ACK                         = 1,
+    /**
+     * @brief   Ping command.
+     */
     Cmd_Ping                        = 2,
+    /**
+     * @brief   Send debug message command.
+     */
     Cmd_DebugMessage                = 3,
+    /**
+     * @brief   Get running mode command.
+     */
     Cmd_GetRunningMode              = 4,
 
-    Cmd_PrepareWriteFirmware        = 10,   /* Bootloader specific, 
-                                               shall always require ACK */
-    Cmd_WriteFirmwarePackage        = 11,   /* Bootloader specific, 
-                                               shall always require ACK */
-    Cmd_WriteLastFirmwarePackage    = 12,   /* Bootloader specific, 
-                                               shall always require ACK */
-    Cmd_ReadFirmwarePackage         = 13,   /* Bootloader specific, 
-                                               shall always require ACK */
-    Cmd_ReadLastFirmwarePackage     = 14,   /* Bootloader specific, 
-                                               shall always require ACK */
-    Cmd_NextPackage                 = 15,   /* Bootloader specific, 
-                                               shall always require ACK */
-    Cmd_ExitBootloader              = 16,   /* Bootloader specific, 
-                                               shall always require ACK */
+    /*===============================================*/
+    /* Bootloader specific commands.                 */
+    /*===============================================*/
+
+    /**
+     * @brief   Prepare to write firmware command.
+     * @note    Bootloader specific, shall always require ACK.
+     */
+    Cmd_PrepareWriteFirmware        = 10,
+    /**
+     * @brief   Write firmware package command.
+     * @note    Bootloader specific, shall always require ACK.
+     */
+    Cmd_WriteFirmwarePackage        = 11,
+    /**
+     * @brief   Write the last firmware package.
+     * @note    Bootloader specific, shall always require ACK.
+     */
+    Cmd_WriteLastFirmwarePackage    = 12,
+    /**
+     * @brief   Read firmware package.
+     * @note    Bootloader specific, shall always require ACK.
+     */
+    Cmd_ReadFirmwarePackage         = 13,
+    /**
+     * @brief   Read the last firmware package.
+     * @note    Bootloader specific, shall always require ACK.
+     */
+    Cmd_ReadLastFirmwarePackage     = 14,
+    /**
+     * @brief   Next package command (sent to PC to resume transmission).
+     * @note    Bootloader specific, shall always require ACK.
+     */
+    Cmd_NextPackage                 = 15,
+    /**
+     * @brief   Exit bootloader command.
+     * @note    Bootloader specific, shall always require ACK.
+     */
+    Cmd_ExitBootloader              = 16,
+
+    /*===============================================*/
+    /* Info, ID and Save commands.                   */
+    /*===============================================*/
+
+    /**
+     * @brief   Get device IDs.
+     */
     Cmd_GetDeviceInfo               = 17,
+    /**
+     * @brief   Set the user definable ID string.
+     */
     Cmd_SetDeviceID                 = 18,
+    /**
+     * @brief   Save all settings to flash.
+     */
     Cmd_SaveToFlash                 = 19,
 
+    /*===============================================*/
+    /* Controller specific commands.                 */
+    /*===============================================*/
+
+    /**
+     * @brief   Get Arm Settings.
+     */
     Cmd_GetArmSettings              = 28,
+    /**
+     * @brief   Set Arm Settings.
+     */
     Cmd_SetArmSettings              = 29,
+    /**
+     * @brief   Get rate controller data.
+     */
     Cmd_GetRateControllerData       = 30,
+    /**
+     * @brief   Set rate controller data.
+     */
     Cmd_SetRateControllerData       = 31,
+    /**
+     * @brief   Get attitude controller data.
+     */
     Cmd_GetAttitudeControllerData   = 32,
+    /**
+     * @brief   Set attitude controller data.
+     */
     Cmd_SetAttitudeControllerData   = 33,
+    /**
+     * @brief   Get velocity controller data.
+     */
     Cmd_GetVelocityControllerData   = 34,
+    /**
+     * @brief   Set velocity controller data.
+     */
     Cmd_SetVelocityControllerData   = 35,
+    /**
+     * @brief   Get position controller data.
+     */
     Cmd_GetPositionControllerData   = 36,
+    /**
+     * @brief   Set position controller data.
+     */
     Cmd_SetPositionControllerData   = 37,
-    /* 38 is reserved! Command 38 + ACK will become SYNC which is forbidden. */
+
+    /*=======================================*/
+    /* 38 is reserved! Command 38 + ACK will */
+    /* become SYNC which is forbidden.       */
+    /*=======================================*/
+
+    /**
+     * @brief   Get channel mixer.
+     */
     Cmd_GetChannelMix               = 39,
+    /**
+     * @brief   Set channel mixer.
+     */
     Cmd_SetChannelMix               = 40,
+
+    /*===============================================*/
+    /* RC specific commands.                         */
+    /*===============================================*/
+
+    /**
+     * @brief   Get RC calibration.
+     */
     Cmd_GetRCCalibration            = 41,
+    /**
+     * @brief   Set RC calibration.
+     */
     Cmd_SetRCCalibration            = 42,
+    /**
+     * @brief   Get RC values.
+     */
     Cmd_GetRCValues                 = 43,
+    
+    /*===============================================*/
+    /* Sensor specific commands.                     */
+    /*===============================================*/
+
+    /**
+     * @brief   Get calibrated sensor data.
+     */
     Cmd_GetSensorData               = 44,
+    /**
+     * @brief   Get raw sensor data.
+     */
     Cmd_GetRawSensorData            = 45,
+    /**
+     * @brief   Get sensor calibration.
+     */
     Cmd_GetSensorCalibration        = 46,
+    /**
+     * @brief   Set sensor calibration.
+     */
     Cmd_SetSensorCalibration        = 47,
+
+    /*===============================================*/
+    /* Estimation specific commands.                 */
+    /*===============================================*/
+
+    /**
+     * @brief   Get rate estimation.
+     */
     Cmd_GetEstimationRate           = 48,
+    /**
+     * @brief   Get attitude estimation.
+     */
     Cmd_GetEstimationAttitude       = 49,
+    /**
+     * @brief   Get velocity estimation.
+     */
     Cmd_GetEstimationVelocity       = 50,
+    /**
+     * @brief   Get position estimation.
+     */
     Cmd_GetEstimationPosition       = 51,
+    /**
+     * @brief   Get all states.
+     */
     Cmd_GetEstimationAllStates      = 52,
+    /**
+     * @brief   Reset estimation.
+     */
     Cmd_ResetEstimation             = 53
 } KFly_Command_Type;
 
-/* The structure to keep track of transfers through the state machine */
+/**
+ * @brief   The structure to keep track of transfers through the state machine.
+ */
 typedef struct _parser_holder
 {
-    Port_Type Port;                                         /* Which port the
-                                                               data came from */
-    bool AckRequested;                                      /* If an ACK was
-                                                               requested      */
-    uint8_t data_length;                                    /* The length of 
-                                                               the data       */
-    uint8_t *buffer;                                        /* Pointer to the 
-                                                               buffer storing 
-                                                               the data       */
-    uint16_t buffer_count;                                  /* The current 
-                                                               location in the 
-                                                               buffer         */
-    uint8_t crc8;                                           /* The current CRC8 
-                                                               calculation    */
-    uint16_t crc16;                                         /* The current CRC16
-                                                               calculation    */
-    uint32_t rx_error;                                      /* The number of 
-                                                               receive errors */
-    void (*current_state)(uint8_t, struct _parser_holder *);/* Current state 
-                                                               in the state 
-                                                               machine        */
-    void (*next_state)(uint8_t, struct _parser_holder *);   /* Next state in 
-                                                               the state
-                                                               machine        */
-    void (*parser)(struct _parser_holder *);                /* Parser to parse 
-                                                               the data after a 
-                                                               successful 
-                                                               transfer       */
+    /**
+     * @brief   Which port the data came from.
+     */
+    Port_Type Port;
+    /**
+     * @brief   If an ACK was requested.
+     */
+    bool AckRequested;
+    /**
+     * @brief   The length of the data.
+     */
+    uint8_t data_length;
+    /**
+     * @brief   Pointer to the buffer storing the data.
+     */
+    uint8_t *buffer;
+    /**
+     * @brief   The current location in the buffer.
+     */
+    uint16_t buffer_count;
+    /**
+     * @brief   The current CRC8 calculation.
+     */
+    uint8_t crc8;
+    /**
+     * @brief   The current CRC16 calculation.
+     */
+    uint16_t crc16;
+    /**
+     * @brief   The number of receive errors.
+     */
+    uint32_t rx_error;
+    /**
+     * @brief   Pointer to the current state in the state machine.
+     */
+    void (*current_state)(uint8_t, struct _parser_holder *);
+    /**
+     * @brief   Pointer to the next state in the state machine.
+     */
+    void (*next_state)(uint8_t, struct _parser_holder *);
+    /**
+     * @brief    Pointer to the parser to parse the data after a
+     *           successful transfer.
+     */
+    void (*parser)(struct _parser_holder *);
 } Parser_Holder_Type;
 
-/* Function pointer definition for the Message Parser lookup table */
+/**
+ * @brief   Function pointer definition for the Message Parser lookup table.
+ */
 typedef void (*Parser_Type)(struct _parser_holder *);
 
-/* Function pointer definition for the Message Generator lookup table */
+/**
+ * @brief   Function pointer definition for the Message Generator lookup table.
+ */
 typedef bool (*Generator_Type)(Circular_Buffer_Type *);
 
+/*===========================================================================*/
+/* Module macros.                                                            */
+/*===========================================================================*/
+
+/*===========================================================================*/
+/* Module inline functions.                                                  */
+/*===========================================================================*/
+
+/*===========================================================================*/
+/* External declarations.                                                    */
+/*===========================================================================*/
 
 #endif
