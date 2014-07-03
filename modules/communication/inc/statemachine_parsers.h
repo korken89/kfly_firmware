8d12761a0ea2e9fa74ca6828d4526fe73ad75e07
Emil Fresk <emil.fresk@gmail.com>
2014-07-03 10:17:25 +0200

    Moved the subscription parser to the correct place

--- modules/communication/inc/statemachine_parsers.h
+++ modules/communication/inc/statemachine_parsers.h
@@ -11,6 +11,30 @@
 /* Module data structures and types.                                         */
 /*===========================================================================*/
 
+/**
+ * @brief   Parsing structure for the management of subscriptions.
+ */
+typedef struct PACKED_VAR
+{
+    /**
+     * @brief   The port on which the command shall be (un)subscribed.
+     */
+    External_Port port;
+    /**
+     * @brief   Which command to (un)subscribe (from)to.
+     */
+    KFly_Command command;
+    /**
+     * @brief   If the command shall be subscribed or unsubscribed.
+     *          0 denotes unsubscribe, anything else is subscribe.
+     */
+    uint8_t on_off;
+    /**
+     * @brief   The time between transmissions of the subscription in ms.
+     */
+    uint32_t delta_time;
+} subscriptions_parser_t;
+
 /*===========================================================================*/
 /* Module macros.                                                            */
 /*===========================================================================*/
