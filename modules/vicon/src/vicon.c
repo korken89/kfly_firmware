c2e84656c0659fada1a25aa1303fd14fe7de4738
Emil Fresk <emil.fresk@gmail.com>
2015-02-12 16:52:19 +0100

    Started adding function structure for Vicon support

--- modules/vicon/src/vicon.c
+++ modules/vicon/src/vicon.c
@@ -19,6 +19,7 @@
 /*===========================================================================*/
 /* Module local variables and types.                                         */
 /*===========================================================================*/
+EVENTSOURCE_DECL(new_vicon_data_es);
 
 /*===========================================================================*/
 /* Module local functions.                                                   */
@@ -27,3 +28,30 @@
 /*===========================================================================*/
 /* Module exported functions.                                                */
 /*===========================================================================*/
+
+/**
+ * @brief   Initializes the New Vicon Data events.
+ */
+void InitializeViconSupport()
+{
+	/* Initialize New Vicon Data event source */
+	osalEventObjectInit(&new_vicon_data_es);
+}
+
+/**
+ * @brief   Broadcast the New Vicon Data events.
+ */
+void vBroadcastNewViconDataAvailable()
+{
+
+}
+
+/**
+ * @brief       Returns the pointer to the New Vicon Data event source.
+ *
+ * @return      Pointer to the New Vicon Data event source.
+ */
+event_source_t *ptrGetViconDataEventSource(void)
+{
+	return &new_vicon_data_es;
+}
