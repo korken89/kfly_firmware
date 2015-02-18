cbd9a39b90cd746f744ab6818879eeb63b020cd6
Emil Fresk <emil.fresk@gmail.com>
2015-02-19 00:06:20 +0100

    Started adding Vicon implementation

--- modules/estimation/inc/vicon_estimator.h
+++ modules/estimation/inc/vicon_estimator.h
@@ -5,6 +5,8 @@
 #include "quaternion.h"
 #include "linear_algebra.h"
 #include "trigonometry.h"
+#include "attitude_ekf.h"
+#include "vicon.h"
 
 /*===========================================================================*/
 /* Module global definitions.                                                */
@@ -25,5 +27,10 @@
 /*===========================================================================*/
 /* External declarations.                                                    */
 /*===========================================================================*/
+void vInitializeViconEstimator(attitude_states_t *states);
+void vInnovateViconEstimator(attitude_states_t *states,
+                             vector3f_t gyro_rate,
+                             const float dt,
+                             const float gyro_lpf);
 
 #endif /* __VICON_ESTIMATOR_H */
--- modules/estimation/src/vicon_estimator.c
+++ modules/estimation/src/vicon_estimator.c
@@ -3,7 +3,8 @@
  *
  * */
 
-#include <math.h>
+#include "ch.h"
+#include "hal.h"
 #include "vicon_estimator.h"
 
 /*===========================================================================*/
@@ -17,6 +18,7 @@
 /*===========================================================================*/
 /* Module local variables and types.                                         */
 /*===========================================================================*/
+static vicon_measurement_t vicon_data;
 
 /*===========================================================================*/
 /* Module local functions.                                                   */
@@ -26,3 +28,20 @@
 /* Module exported functions.                                                */
 /*===========================================================================*/
 
+void vInitializeViconEstimator(attitude_states_t *states)
+{
+    /* Wait while there is no valid data from the Vicon system. */
+    do
+    {
+        chThdSleepMilliseconds(10);
+        GetCopyViconMeasurement(&vicon_data);
+    } while (vicon_data.frame_number == 0);
+}
+
+void vInnovateViconEstimator(attitude_states_t *states,
+                             vector3f_t gyro_rate,
+                             const float dt,
+                             const float gyro_lpf)
+{
+
+}
