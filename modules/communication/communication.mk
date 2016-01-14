cd42a060f3c932621edb88fc154faf976aa66a88
Emil Fresk <emil.fresk@gmail.com>
2016-01-14 18:45:25 +0100

    Added skeleton for SLIP support

--- modules/communication/communication.mk
+++ modules/communication/communication.mk
@@ -1,5 +1,6 @@
 # List of all the module's related files.
 COMMUNICATION_SRCS = $(MODULE_DIR)/communication/src/circularbuffer.c \
+                     $(MODULE_DIR)/communication/src/slip.c \
                      $(MODULE_DIR)/communication/src/serialmanager.c \
                      $(MODULE_DIR)/communication/src/subscriptions.c \
                      $(MODULE_DIR)/communication/src/statemachine_generators.c \
