0ef0242c1e3286ded797fbf2d121a5b1685ba8ae
Emil Fresk <emil.fresk@gmail.com>
2016-01-20 16:42:29 +0100

    Updated the info text for the communication

--- modules/communication/src/slip2kflypacket.c
+++ modules/communication/src/slip2kflypacket.c
@@ -3,23 +3,16 @@
  * OS layer for Serial Communication.
  * Handles package coding/decoding.
  *
- * THE TEXT BELLOW IS NOT VALID ANYMORE! CURRENTLY RUNNING SLIP.
  *
  * Serial Communication Protocol
  * -----------------------------
- * This was designed so that binary data could be sent while not
- * needing to "code" it as ASCII HEX. A simple SYNC byte is used
- * to denote the start of a transfer and after that a header
- * containing the command and size of the message. If the size is
- * greater than 0 a data package comes after the first CRC. If
- * the data contains a byte that has the same value as the sync
- * byte it will be replaced by two sync bytes "x" -> "xx" to denote
- * a byte of the value sync and not a data package sync.
- *
+ * The KFly is using a packet structure encoded inside a Serial Line Internet
+ * Protocol (SLIP) to transfer data packets. The packet structure used inside
+ * the SLIP packet is described bellow.
  *
  * Protocol:
- *      SYNC | HEADER | CRC8 | DATA | CRC16
- *      DATA and CRC16 is optional.
+ *      HEADER | DATA | CRC16
+ *      DATA is optional.
  *
  * HEADER:
  *      CMD     | DATA SIZE
@@ -29,15 +22,8 @@
  *      BINARY DATA
  *      1 - 255 bytes
  *
- * SYNC: 1 byte
- *      Sent once = SYNC
- *      Sent twice = data byte with the value of SYNC ("escaping")
- *
- * CRC8: 1 byte
- *      CRC-8 of SYNC and HEADER
- *
  * CRC16: 2 bytes
- *      CCITT (16-bit) of whole message including SYNC and CRC8
+ *      CCITT (16-bit) of whole message including header.
  *      For more information about the CRCs look in crc.c/crc.h
  *
  *
@@ -45,9 +31,8 @@
  * A command is build up by 8 bits (1 byte) and the 8-th bit (MSB) is the ACK
  * request bit. So all command must not use the ACK bit unless they need an ACK.
  * Command 0bAxxx xxxx <- A is ACK-bit.
- * Also the value 0xa6/166d/0b10100110 is reserved as the SYNC-byte.
  *
- * This gives 126 commands for the user.
+ * This gives 127 commands for the user.
  *
  */
 
