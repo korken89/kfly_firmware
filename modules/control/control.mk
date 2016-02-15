4c08b19eea0f36c4824c928966ba75348d88585f
Emil Fresk <emil.fresk@gmail.com>
2016-02-15 13:31:30 +0100

    Removed unused files (will be re-added in the future)

--- modules/control/control.mk
+++ modules/control/control.mk
@@ -3,7 +3,6 @@ CONTROL_SRCS = $(MODULE_DIR)/control/src/control.c \
                $(MODULE_DIR)/control/src/computer_control.c \
                $(MODULE_DIR)/control/src/rate_loop.c \
                $(MODULE_DIR)/control/src/attitude_loop.c \
-               $(MODULE_DIR)/control/src/position_loop.c \
                $(MODULE_DIR)/control/src/arming.c \
                $(MODULE_DIR)/control/src/pid.c
 
