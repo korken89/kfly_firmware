434f884efff3da10ce9f5dda3a9090a559d73979
Emil Fresk <emil.fresk@gmail.com>
2016-02-15 13:37:41 +0100

    Added the control reference skeleton

--- modules/control/control.mk
+++ modules/control/control.mk
@@ -1,5 +1,6 @@
 # List of all the module's related files.
 CONTROL_SRCS = $(MODULE_DIR)/control/src/control.c \
+               $(MODULE_DIR)/control/src/control_reference.c \
                $(MODULE_DIR)/control/src/computer_control.c \
                $(MODULE_DIR)/control/src/rate_loop.c \
                $(MODULE_DIR)/control/src/attitude_loop.c \
