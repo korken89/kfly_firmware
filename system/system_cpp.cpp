c7c20459f34786a7c5383e2b3010a89f69bbd3cb
Emil Fresk <emil.fresk@gmail.com>
2016-06-27 22:18:07 +0200

    Added check for delete operator

--- system/system_cpp.cpp
+++ system/system_cpp.cpp
@@ -6,6 +6,11 @@
 void you_tried_to_use_new(void);
 
 /**
+ * @brief   Define non-existing function to catch the usage of new.
+ */
+void you_tried_to_use_delete(void);
+
+/**
  * @brief   Redefinition of new to stop the usage of it.
  */
 void *operator new(std::size_t size) throw()
@@ -21,6 +26,7 @@ void *operator new(std::size_t size) throw()
 void operator delete(void *p) throw()
 {
     (void)p;
+    you_tried_to_use_delete();
 }
 
 /**
@@ -30,4 +36,5 @@ void operator delete(void *p, std::size_t size) throw()
 {
     (void)p;
     (void)size;
+    you_tried_to_use_delete();
 }
