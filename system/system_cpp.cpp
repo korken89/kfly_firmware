94946346249829b3fbecdcc0442a0c7fbfef7e2d
Emil Fresk <emil.fresk@gmail.com>
2016-06-27 23:02:54 +0200

    Added all variations of new and delete

--- system/system_cpp.cpp
+++ system/system_cpp.cpp
@@ -1,4 +1,5 @@
 #include <cstddef>
+#include <new>
 
 /**
  * @brief   Define non-existing function to catch the usage of new.
@@ -13,7 +14,37 @@ void you_tried_to_use_delete(void);
 /**
  * @brief   Redefinition of new to stop the usage of it.
  */
-void *operator new(std::size_t size) throw()
+void *operator new(std::size_t size) throw(std::bad_alloc)
+{
+    (void) size;
+    you_tried_to_use_new();
+    return NULL;
+}
+
+/**
+ * @brief   Redefinition of new to stop the usage of it.
+ */
+void *operator new(std::size_t size, std::nothrow_t const &) throw()
+{
+    (void) size;
+    you_tried_to_use_new();
+    return NULL;
+}
+
+/**
+ * @brief   Redefinition of new to stop the usage of it.
+ */
+void *operator new [](std::size_t size) throw (std::bad_alloc)
+{
+    (void) size;
+    you_tried_to_use_new();
+    return NULL;
+}
+
+/**
+ * @brief   Redefinition of new to stop the usage of it.
+ */
+void *operator new [] (std::size_t size, std::nothrow_t const &) throw ()
 {
     (void) size;
     you_tried_to_use_new();
@@ -23,7 +54,7 @@ void *operator new(std::size_t size) throw()
 /**
  * @brief   Redefinition of delete to stop the usage of it.
  */
-void operator delete(void *p) throw()
+void operator delete(void *p) throw(std::bad_alloc)
 {
     (void)p;
     you_tried_to_use_delete();
@@ -32,7 +63,26 @@ void operator delete(void *p) throw()
 /**
  * @brief   Redefinition of delete to stop the usage of it.
  */
-void operator delete(void *p, std::size_t size) throw()
+void operator delete(void *p, std::nothrow_t const &) throw()
+{
+    (void)p;
+    you_tried_to_use_delete();
+}
+
+/**
+ * @brief   Redefinition of delete to stop the usage of it.
+ */
+void operator delete(void *p, std::size_t size) throw(std::bad_alloc)
+{
+    (void)p;
+    (void)size;
+    you_tried_to_use_delete();
+}
+
+/**
+ * @brief   Redefinition of delete to stop the usage of it.
+ */
+void operator delete(void *p, std::size_t size, std::nothrow_t const &) throw()
 {
     (void)p;
     (void)size;
