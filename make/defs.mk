775392d4f7e580da8989f1dc508f0984a83d6e6e
Emil Fresk <emil.fresk@gmail.com>
2018-05-06 15:29:35 +0200

    More makefiles

--- make/defs.mk
+++ make/defs.mk
@@ -85,3 +85,47 @@ $(1):  $(2)
 	@echo $(MSG_LINKING) $$@
 	$(V0) $(GXX) $$(CFLAGS) $(2) --output $$@ $$(LDFLAGS) -lm -lc
 endef
+
+all: build
+
+build: dirs elf bin hex lss sym size
+
+# Generate object files
+ALLSRC  = $(ASRCS) $(CSRCS) $(CPPSRCS)
+ALLSRCBASE = $(notdir $(basename $(ALLSRC)))
+ALLOBJECTS = $(addprefix $(OBJDIR)/, $(addsuffix .o, $(ALLSRCBASE)))
+
+# Link: Create elf output file from object files.
+$(eval $(call LINK_TEMPLATE, $(ELFDIR)/$(TARGET).elf, $(ALLOBJECTS)))
+
+# Assemble: Create object files from assembler source files.
+$(foreach src, $(ASRCS), $(eval $(call ASSEMBLE_TEMPLATE, $(src))))
+
+# Compile: Create object files from C source files.
+$(foreach src, $(CSRCS), $(eval $(call COMPILE_C_TEMPLATE, $(src))))
+
+# Compile: Create object files from C++ source files.
+$(foreach src, $(CPPSRCS), $(eval $(call COMPILE_CPP_TEMPLATE, $(src))))
+
+dirs:
+	$(V0) mkdir -p $(OBJDIR)
+	$(V0) mkdir -p $(DEPDIR)
+	$(V0) mkdir -p $(ELFDIR)
+
+elf: $(ELFDIR)/$(TARGET).elf
+lss: $(ELFDIR)/$(TARGET).lss
+sym: $(ELFDIR)/$(TARGET).sym
+hex: $(ELFDIR)/$(TARGET).hex
+bin: $(ELFDIR)/$(TARGET).bin
+
+size: $(ELFDIR)/$(TARGET).elf
+	$(V0) echo $(MSG_SIZE) $(TARGET).elf
+	$(V0) $(SIZE) -A $(ELFDIR)/$(TARGET).elf
+
+dump: $(ELFDIR)/$(TARGET).elf
+	$(V0) echo $(MSG_DUMP) $(TARGET).elf
+	$(V0) $(OBJDUMP) -D $(ELFDIR)/$(TARGET).elf > $(ELFDIR)/$(TARGET)_dump.txt
+
+clean:
+	$(V0) echo $(MSG_CLEANING) $(BUILDDIR)
+	$(V0) rm -rf $(BUILDDIR)
